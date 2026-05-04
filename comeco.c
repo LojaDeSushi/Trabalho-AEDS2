0c5fe8ee4ddaa8c8cd5295e9c2977794eb4099b3
LojaDeSushi <affonso.giulia@gmail.com>
2026-05-03 21:10:58 -0300

    Update comeco.c

--- comeco.c
+++ comeco.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "struct.h"
+#include <Windows.h>
 
 int main(){
+
+    printf("Coloque as infos necessarias para a criacao do grafo: vertices e arestas e depois os pares com os pesos:\n");
 
     int vertices = 0;
     scanf("%d", &vertices);
@@ -12,53 +15,14 @@ int main(){
 
     grafo* grafo = CriaGrafo(vertices);
     int** matrix = CriaMatrix(vertices);
+    
+    CriArestas(grafo, matrix, arestas);
 
-    for(int i = 0; i < arestas; i++){
-        int a;
-        int b;
-        int peso;
-        scanf("%d", &a);
-        scanf("%d", &b);
-        scanf("%d", &peso);
-
-        Arestas(grafo, a, b);
-
-        matrix[a][b] = peso;
-        matrix[b][a] = peso;
-    }
+    int* criticos = NodesCritcos(grafo);
 
     printf("\nEbaa!! O grafo feito com sucesso! :D\nPossui %d nos e %d arestas\n", vertices, arestas);
 
-    //pesos
-    printf("\nMatrix de pesos:\n");
-    PrintaMatrix(grafo, matrix);
-
-    //topografia grafo
-    PrintaGrafo(grafo, matrix);
-
-    //caminho de a ate b
-    for(int i = 0; i < vertices; i++){
-        CaminhoDks(grafo, i, vertices - 1, matrix);
-        printf("\n");
-    }
-
-    //grau de a
-    printf("Grau de %d: ", 2);
-    printf("%d\n", CalculaGrau(grafo, 2));
-
-    //grau medio
-    printf("Media de graus: %.2f\n", Media(grafo));
-
-    //maior grau
-    printf("O maior grau eh: %d\n", Max(grafo));
-
-    
-    //quantos enlaces para desconectar
-    DesconectArest(grafo);
-
-    Agrupa(grafo, 0, matrix);
-    
-    //Mata(grafo, 4, matrix);
+    Pergunta(grafo, matrix, criticos);
 
     return 0;
 }
