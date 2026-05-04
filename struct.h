9b84a2bc528fb210e3f21d1917266815040a8707
LojaDeSushi <affonso.giulia@gmail.com>
2026-05-03 21:19:28 -0300

    Update struct.h

--- struct.h
+++ struct.h
@@ -1,6 +1,8 @@
 #ifndef GRAFO_S
 #define GRAFO_S
 
+#define MAX 100
+
 typedef struct node{
     int vertice;
     struct node* adj;
@@ -16,6 +18,8 @@ node* CriaNo(int num);
 grafo* CriaGrafo(int vertices);
 
 void Arestas(grafo* grafo, int a, int b);
+
+void CriArestas(grafo* grafo, int** matrix, int arestas);
 
 void PrintaGrafo(grafo* grafo, int** matrix);
 
@@ -39,11 +43,26 @@ int Min(grafo* grafo);
 
 void DesconectArest(grafo* grafo);
 
+void BuscArticula(grafo* grafo, int a, int pai);
+
+int Conexo(grafo* grafo, int conte);
+
+int Completo(grafo* grafo);
+
+int* NodesCritcos(grafo* grafo);
+
+void DesconectNode(grafo* grafo, int* criticos);
+
+void PrintaCriticos(grafo* grafo, int* criticos);
+
 void Agrupa(grafo* grafo, int a, int** matrix);
 
-void Mata(grafo* grafo, int a, int** matrix);
+void Mata(grafo* grafo, int a, int** matrix, int* criticos);
 
-void Recalcular(grafo* grafo, int** matrix);
+void Recalcular(grafo* grafo, int** matrix, int* criticos);
 
+int Menu();
+
+void Pergunta(grafo* grafo, int** matrix, int* criticos);
 
 #endif 
